c896111f84e630c2d2553020e0c37b962d5a7a77
Mahmoud-Barry <mahmoudbarrysn@gmail.com>
2022-05-21 16:01:38 +0200

    markdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
+import React, { useState } from "react"
 import logo from './logo.svg';
 import './App.css';
+import {marked} from "marked";
 
 function App() {
+
+  const [text, setText] = useState("")
+  const markdown = marked(text)
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container py-5'>
+        <div className="input">
+            <textarea className='form-control' id="editor" cols="30" rows="10" onChange={e=>setText(e.target.value)}></textarea>
+        </div>
+        <div className="output">
+            <div className="p-5">
+            <article dangerouslySetInnerHTML={{__html: markdown}}></article>
+            </div>
+        </div>
     </div>
   );
 }
